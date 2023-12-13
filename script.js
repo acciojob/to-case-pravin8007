93e4ddf9cd68d4af7ad0cad9775bd0a105dce057
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-12-13 13:37:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function toCase(text) {
   // write your code here
+	const upper = text.toUpperCase();
+	const lower = text.toLowerCase();
+	const ans = lower+"-"+upper;
+	return ans ;
 }
 
 // DO not change the code below
